aca95b068dbeb08f423fb8de03c998f42c18fc49
Drewfus <drewfus.d@gmail.com>
2026-01-05 17:31:28 +1100

    Implement WeatherAPI module: add functions for fetching and parsing weather data; include mock data generation and utility functions

---
+++ src/WeatherAPI.h
@@ -0,0 +1,24 @@
+#ifndef WEATHER_API_H
+#define WEATHER_API_H
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "Display.h"
+
+// Declare external global variables that these functions modify
+extern WeatherData currentWeather;
+extern DailyForecast dailyForecasts[3];
+extern HourlyData hourlyData[24];
+
+// Function declarations
+void getMockForecastData();
+String getIconNameFromUri(String uri);
+String getAPIData(String url);
+void updateCurrentWeather(JsonObject hourly);
+String getDayName(int year, int month, int day);
+void getDailyForecastData();
+void getHourlyForecastData(int hoursCount);
+void getHistoryData(int hoursCount);
+void getWeatherCurrentData();
+
+#endif
